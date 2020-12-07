e80d8e76110a835b680517fa19eae209c9a15da9
Ivan Stanev <ivan@stanev.me>
2020-12-07 14:02:40 +0100

    without data organization

--- services/aws/4-eks/data.tf
+++ services/aws/4-eks/data.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-data "aws_organizations_organization" "current" {}
+# data "aws_organizations_organization" "current" {}
 
 data "aws_availability_zones" "available" {
   state = "available"
