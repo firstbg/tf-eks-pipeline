549fd3fd6dd07d8c0756c84fc01457fbc5e574c3
Ivan Stanev <ivan@stanev.me>
2020-12-07 12:54:08 +0100

    without data organization

--- services/aws/3-vpc-network/data.tf
+++ services/aws/3-vpc-network/data.tf
@@ -1,6 +1,6 @@
 data "aws_region" "current" {}
 
-data "aws_organizations_organization" "current" {}
+# data "aws_organizations_organization" "current" {}
 
 data "aws_availability_zones" "available" {
   state = "available"
